61435355f7f1b8c4ac664a7374f2ef84f6796e80
Gomathi799 <46838489+Gomathi799@users.noreply.github.com>
2019-01-20 11:33:42 +0530

    primeinterval.c

---
+++ primeinterval.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,i,j,flag=0;
+    scanf("%d%d",&a,&b);
+    for(i=a+1;i<b;i++)
+    {
+        flag=0;
+        for(j=2;j<=i/2;j++)
+        {
+            if(i%j==0)
+            {
+                flag=1;
+                break;
+            }
+        }
+        if(flag==0)
+        printf("%d ",i);
+    }
+}
