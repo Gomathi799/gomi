eb7d5048b939449eb75b8ba31ab68ebe353a4dfc
Gomathi799 <46838489+Gomathi799@users.noreply.github.com>
2019-01-20 10:15:26 +0530

    arraysum.c

---
+++ arraysum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int n,k,a[100],i,j,sum=0;
+    scanf("%d%d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+    scanf("%d",&a[i]);
+    }
+    for(i=0;i<k;i++)
+    {
+        sum=sum+a[i];
+    }
+    printf("%d",sum);
+}
