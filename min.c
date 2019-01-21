d7fe949841881ca3ae318e8df21344b4a11b5997
Gomathi799 <46838489+Gomathi799@users.noreply.github.com>
2019-01-21 20:42:10 +0530

    min.c

---
+++ min.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int n,a[100],min,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    min=a[0];
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<min)
+        {
+            min=a[i];
+        }
+    }
+    printf("%d",min);
+}
