deea059bf0965bded06baee632157e4f65dd0162
Gomathi799 <46838489+Gomathi799@users.noreply.github.com>
2019-01-21 21:11:42 +0530

    mediann.c

---
+++ mediann.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+    int n,a[100],i,t,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+        if(a[i]>a[j])
+        {
+            t=a[i];
+            a[i]=a[j];
+            a[j]=t;
+        }
+        }
+    }
+    a[n]='\0';
+    i=n/2;
+    printf("%d",a[i]);
+}
