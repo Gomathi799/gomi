9d56a1363864e8c828c997b057f101770e5835ae
Gomathi799 <46838489+Gomathi799@users.noreply.github.com>
2019-01-21 20:34:42 +0530

    max.c

---
+++ max.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int n,a[100],max=0,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+        }
+    }
+    printf("%d",max);
+}
