b00f210921096dff835b79e41496ef4120797d0b
Gomathi799 <46838489+Gomathi799@users.noreply.github.com>
2019-01-22 21:28:34 +0530

    strlen.c

---
+++ strlen.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    char a[100];
+    int m=0,i;
+    gets(a);
+    for(i==0;a[i]!='\0';i++)
+    {
+        if(a[i]!=' ')
+        {
+            m++;
+        }
+    }
+    printf("%d",m);
+}
