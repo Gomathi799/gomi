8ec3d52fd4d4cd8e391c97955923046b67eab66c
Gomathi799 <46838489+Gomathi799@users.noreply.github.com>
2019-02-05 20:07:29 +0530

    6_7.c

---
+++ 6_7.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int a[100],i,n,k,m=0;
+    scanf("%d%d",&n,&k);
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==k)
+        {
+            m++;
+        }
+    }
+    printf("%d",m);
+}
