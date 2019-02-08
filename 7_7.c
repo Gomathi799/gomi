c5a7a55c16e2e761bf9a11289c77fea10e80cce7
Gomathi799 <46838489+Gomathi799@users.noreply.github.com>
2019-02-08 19:08:22 +0530

    7_7.c

---
+++ 7_7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int n,i;
+    scanf("%d",&n);
+    for(i=n;i>0;i++)
+    {
+      if(i%10==0)
+      {
+          printf("%d",i);
+          break;
+      }
+    }
+}
