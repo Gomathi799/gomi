f0b439831fb0f6651ade67a6388a5383a4df350e
Gomathi799 <46838489+Gomathi799@users.noreply.github.com>
2019-02-11 18:46:27 +0530

    9_33.c

---
+++ 9_33.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    char sym;
+    scanf("%d%c%d",&a,&sym,&b);
+    if(sym=='/')
+    printf("%d",a/b);
+    else
+    printf("%d",a%b);
+    
+}
+
+
